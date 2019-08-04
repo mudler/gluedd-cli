adb6c0da12f02c1656b7bcf4b92c93c31c791c64
Ettore Di Giacinto <mudler@gentoo.org>
2019-08-05 00:45:54 +0200

    Add extra prediction

--- pkg/jobs/dummy.go
+++ pkg/jobs/dummy.go
@@ -25,6 +25,22 @@ func (e *DummyErrand) Apply() error {
 	return nil
 }
 
+// Generate makes the errand generates new prediction if needed
+func (e *DummyErrand) Generate(d api.Detector) *api.Prediction {
+	for _, c := range e.Prediction.Body.Predictions[0].Classes {
+		if strings.Contains(c.Cat, "Van") || strings.Contains(c.Cat, "Truck") || strings.Contains(c.Cat, "Car")  || strings.Contains(c.Cat, "Tree") {
+
+			fmt.Println("Extra generation!")
+			pre := d.DetectService(e.Prediction.Url,"ilsvrc_googlenet")
+			return &pre
+		}
+	}
+
+
+	return nil
+}
+
+
 type DummyGenerator struct{}
 
 func NewDummyGenerator() errand.ErrandGenerator {
