1620d96574529b0f69f786921f51c9c4f4ecd086
Ettore Di Giacinto <mudler@gentoo.org>
2020-08-09 12:09:48 +0200

    Add ohab processor for yolo to cli

---
+++ cmd/yolo-mjpeg-ohab.go
@@ -0,0 +1,86 @@
+// Copyright © 2019 Ettore Di Giacinto <mudler@gentoo.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	generators "github.com/mudler/gluedd-cli/pkg/jobs"
+	types "github.com/mudler/gluedd-cli/pkg/resource"
+	live "github.com/saljam/mjpeg"
+
+	"time"
+
+	"github.com/mudler/gluedd/pkg/errand"
+	"github.com/mudler/gluedd/pkg/predictor"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var yoloMJpegOhab = &cobra.Command{
+	Use:   "yolo-mjpeg-ohab",
+	Short: "Uses Yolo http api to process mjpeg streams to openhab",
+	Long:  `Reads from a jpeg snapshot url and does live detection`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		Server := viper.GetString("api_server")
+
+		dd := &generators.YoloCV{Server: Server}
+
+		stream := live.NewStream()
+
+		opts := types.MJpegStreamerOptions{
+			ListeningURL:     viper.GetString("base_url"),
+			LiveStreamingURL: viper.GetString("stream_url"),
+			Stream:           stream,
+			LivePreview:      viper.GetBool("preview"),
+			Buffer:           viper.GetInt("buffer_size"),
+			Width:            uint(viper.GetInt("image_width")),
+			Height:           uint(viper.GetInt("image_height")),
+			Resize:           viper.GetBool("resize"),
+			Approx:           viper.GetBool("approx"),
+			Timeout:          viper.GetInt("client_timeout"),
+			Crop:             viper.GetBool("crop"),
+			CropAnchor:       viper.GetBool("crop_anchor"),
+			CropMode:         viper.GetString("crop_mode"),
+			CropAnchorX:      viper.GetInt("crop_anchor_x"),
+			CropAnchorY:      viper.GetInt("crop_anchor_y"),
+			CropWidth:        viper.GetInt("crop_width"),
+			CropHeight:       viper.GetInt("crop_height"),
+		}
+
+		openhabOptions := &generators.OpenHabGeneratorOptions{
+			APIURL:      viper.GetString("openhab_url"),
+			VehicleItem: viper.GetString("vehicle_item"),
+			HumanItem:   viper.GetString("person_item"),
+			AnimalItem:  viper.GetString("animal_item"),
+			Stream:      stream,
+			Live:        viper.GetBool("preview"),
+		}
+
+		errandgen := generators.NewOpenHabGenerator(openhabOptions)
+
+		predictor := predictor.NewPredictor(dd, types.NewMJpegStreamer(opts), errandgen)
+		consumer := errand.NewErrandConsumer()
+
+		consumer.Consume(predictor.Generate())
+		for {
+			time.Sleep(1 * time.Second)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(yoloMJpegOhab)
+}
